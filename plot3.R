a440294d5a840cd044058b1a57ecbf2798c4a43e
melizh <mehld26@gmail.com>
2022-12-14 19:53:01 -0800

    Plot 3 done

---
+++ plot3.R
@@ -0,0 +1,19 @@
+# Load data
+rm(list=ls())
+library(dplyr)
+df <- read.delim('./exdata_data_household_power_consumption/household_power_consumption.txt',sep=';')
+df <- df[(df$Date == "1/2/2007") | (df$Date == "2/2/2007"),]
+df$Global_active_power <- as.numeric(df$Global_active_power)
+df$Sub_metering_1 <- as.numeric(df$Sub_metering_1)
+df$Sub_metering_2 <- as.numeric(df$Sub_metering_2)
+df$Sub_metering_3 <- as.numeric(df$Sub_metering_3)
+df <- mutate(df,dateandtime = as.POSIXct(paste(df$Date,df$Time),format="%d/%m/%Y %H:%M:%S"))
+
+png(filename='plot3.png')
+plot(df$dateandtime,df$Sub_metering_1,type='l',
+     xlab='',
+     ylab='Energy sub metering')
+points(df$dateandtime,df$Sub_metering_2,type='l',col='orangered')
+points(df$dateandtime,df$Sub_metering_3,type='l',col='royalblue')
+legend("topright",lty=c(1,1,1), col=c('black','orangered','royalblue'), legend=names(df)[7:9])
+dev.off()
